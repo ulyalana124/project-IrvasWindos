1bdc3e9bcfaca7b5249dd9ad077199ec9c192dfa
Uliana <ulanasadikova65@gmail.com>
2021-02-19 18:53:08 +0200

    Add timer, images module

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,14 @@ import modals from './modules/modals';
 import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState';
+import timer from './modules/timer';
+import images from './modules/images';
 
 window.addEventListener('DOMContentLoaded', () => {
    "use strict";
 
    let modalState = {};
+   let deadline = '2021-08-12';
 
    changeModalState(modalState);
    modals();
@@ -15,6 +18,8 @@ window.addEventListener('DOMContentLoaded', () => {
    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
    forms(modalState);
+   timer('.container1', deadline);
+   images();
 });
 
 console.log(1);
